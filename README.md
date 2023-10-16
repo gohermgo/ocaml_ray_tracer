08cc34be3a5526186c7e0147dc01796e5eb3d9b2
gohermgo <gohnherman@gmail.com>
2023-10-16 20:10:47 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# The Ocaml Ray Tracer
---
+++ lib/tuples.mli
@@ -0,0 +1,32 @@
+type t = { x: float; y: float; z: float; w: float }
+
+val equal: t -> t -> bool
+
+val init: (float * float * float * float) -> t
+
+val point: (float * float * float) -> t
+
+val is_point: t -> bool
+
+val vector: (float * float * float) -> t
+
+val is_vector: t -> bool
+
+val add: t -> t -> t
+
+(* Per element difference *)
+val sub: t -> t -> t
+
+val neg: t -> t
+
+val mul: t -> float -> t
+
+val div: t -> float -> t
+
+val mag: t -> float
+
+val norm: t -> t
+
+val dot: t -> t -> float
+
+val cross: t -> t -> t
